81a3fa74a9c38719940185d8baf136a5d3b4cafb
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2023-01-19 17:55:14 +0200

    Add App jsx, states and change handler

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,40 @@
+import { ChangeEvent, useState } from "react";
+
 function App() {
+  const [task, setTask] = useState<string>("");
+  const [deadline, setDeadline] = useState<number>(0);
+  const [todo, setTodoList] = useState([]);
+
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.name === "task") {
+      setTask(e.target.value);
+    } else {
+      setDeadline(Number(e.target.value));
+    }
+  };
+
   return (
     <div className="App">
-     Helllo
+      <div className="header">
+        <div className="inputContainer">
+          <input
+            type="text"
+            placeholder="Task..."
+            name="task"
+            value={task}
+            onChange={handleChange}
+          />
+          <input
+            type="number"
+            placeholder="Deadline (in Days)..."
+            name="deadline"
+            value={deadline}
+            onChange={handleChange}
+          />
+        </div>
+        <button>Add Task</button>
+      </div>
+      <div className="todoList"></div>
     </div>
   );
 }
